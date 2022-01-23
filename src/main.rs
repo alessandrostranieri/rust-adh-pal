98be15f654b3cd42a5f1872a4137b176f3d5a438
Alessandro Stranieri <alessandro.stranieri@gmail.com>
2022-01-23 13:50:56 +0100

    Upgrade dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,48 @@
 extern crate clap;
 
-use clap::{App, SubCommand};
+use clap::{arg, App, AppSettings};
 
 mod cmd;
 
 fn main() {
-    let matches = App::new("My ADHD Pal")
+    let matches = App::new("adh-pal")
         .version("0.1.0")
         .author("Alessandro Stranieri <alessandro.stranieri@gmail.com>")
         .about("Your pal against ADHD")
-        .subcommand(SubCommand::with_name("start"))
-        .subcommand(SubCommand::with_name("stop"))
-        .subcommand(SubCommand::with_name("current"))
-        .subcommand(SubCommand::with_name("list"))
-        .subcommand(SubCommand::with_name("mood"))
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            App::new("start")
+                .about("Start an activity")
+                .arg(arg!(<ACTIVITY> "The activity to start"))
+                .setting(AppSettings::ArgRequiredElseHelp),
+        )
+        .subcommand(App::new("stop").about("Start an activity"))
+        .subcommand(
+            App::new("current")
+                .about("Display the current activity")
+                .arg(
+                    arg!(-c --clear "Clear the current activity.")
+                ),
+        )
+        .subcommand(
+            App::new("list")
+                .about("List all completed activities")
+                .arg(arg!(<NUM> "Number of activities")),
+        )
+        .subcommand(
+            App::new("mood")
+            .about("Record the current mood")
+            .arg(arg!(<MOOD> "Clear the current activity"))
+            .setting(AppSettings::ArgRequiredElseHelp),
+        )
         .get_matches();
 
     match matches.subcommand() {
-        ("stop", Some(_)) => cmd::stop(),
-        ("start", Some(_)) => cmd::start(),
-        ("current", Some(_)) => cmd::current(),
-        ("list", Some(_)) => cmd::list(),
-        ("mood", Some(_)) => cmd::mood(),
+        Some(("start", _)) => cmd::start(),
+        Some(("stop", _)) => cmd::stop(),
+        Some(("current", _)) => cmd::current(),
+        Some(("list", _)) => cmd::list(),
+        Some(("mood", _)) => cmd::mood(),
         _ => {} // TODO print help
     }
 }
